8c744368d210ceabd2efa94acc8f208f11518d56
Shiqiang <cocowool@qq.com>
2022-06-05 14:29:28 +0800

    modify mp3tag code

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-from setuptools import setup, find_packages
+import setuptools
+# from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
 DESCRIPTION = 'MP3 tag information reader and writer.'
 LONG_DESCRIPTION = 'MP3 contains many information about the music and stored in the tag. This package support read and write id3v2 tag to the mp3 file.'
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
     name='mp3tag',
     version=VERSION,
     author='Wang Shiqiang',
     author_email='cocowool@qq.com',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cocowool/mp3tag",
+    project_urls={
+        "Bug Tracker":"https://github.com/cocowool/mp3tag/issues"
+    },
+    package_dir={"":"src"}
+    packages=setuptools.find_packages(where="src"),
     install_requires=[],
 
     keywords=['mp3','id3','id3v2'],
-    classifiers=[]
+    classifiers=[],
+    python_requires=">=3.6"
 )
